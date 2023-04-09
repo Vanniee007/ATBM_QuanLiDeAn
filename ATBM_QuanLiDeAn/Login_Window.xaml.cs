d9ab6b3a62c9bb43040698d2aafd116c912c4252
Vanniee <87258954+Vanniee007@users.noreply.github.com>
2023-04-09 19:58:39 +0700

    done

--- ATBM_QuanLiDeAn/Login_Window.xaml.cs
+++ ATBM_QuanLiDeAn/Login_Window.xaml.cs
@@ -79,10 +79,10 @@ namespace ATBM_QuanLiDeAn
         }
         private void Login()
         {
-            //string username = tb_username.Text.ToString();
-            //string password = tb_password.Password.ToString();
-            string username = "ATBMM";
-            string password = "123";
+            string username = tb_username.Text.ToString();
+            string password = tb_password.Password.ToString();
+            //string username = "ATBMM";
+            //string password = "123";
 
             bool isConnect = Class.DB_Config.Connect(username, password);
             if (isConnect)  // Kết nối thành công
