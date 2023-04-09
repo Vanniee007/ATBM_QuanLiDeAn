e77361cf82f88b693cb7b2414b551ea2f6458df7
minhTienn2602 <96586521+minhTienn2602@users.noreply.github.com>
2023-04-09 19:11:58 +0700

    coi như xong bên role

--- ATBM_QuanLiDeAn/PH1/Role_XemQuyen.xaml.cs
+++ ATBM_QuanLiDeAn/PH1/Role_XemQuyen.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,21 +25,145 @@ namespace ATBM_QuanLiDeAn.PH1
         {
             InitializeComponent();
             role = role_;
-        }
-
-        private void Xemquyen_tab_loaded(object sender, RoutedEventArgs e)
-        {
 
         }
-
-        private void Xemquyen_sys_loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+        public void role_xemquyen_hienThi()
+        {
+            try
+            {
+                DataTable tbl = new DataTable();
+                string sql = "select* from ROLE_SYS_PRIVS where ROLE='"+role+"'";
+                tbl = Class.DB_Config.GetDataToTable(sql);
+                Role_xq_sys_Datagrid.ItemsSource = null;
+                Role_xq_sys_Datagrid.ItemsSource = tbl.DefaultView;
+                Role_xq_sys_lb.Content = "Quyền trên hệ thống của role: " + role;
+
+            }
+            catch { }
+        }
+        private void Xemquyen_tab_loaded(object sender, RoutedEventArgs e)
+        {
+            role_xemquyen_hienThi();
+        }
+
+        private void Role_xq_sys_Datagrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            role_xemquyen_hienThi();
+        }
+
+        private void role_xq_bt_quaylai_click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void role_xq_bt_lammoi_click(object sender, RoutedEventArgs e)
+        {
+            role_xemquyen_hienThi();
+        }
+        private void role_xq_bt_xoa_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Role_xq_sys_Datagrid.SelectedIndex.ToString() != null)
+                {
+                    //DataRowView rowview = (DataRowView)Role_xq_sys_Datagrid.SelectedItem;
+                    //if (rowview != null)
+                    //{
+                    //    if (MessageBox.Show("Xác nhận xoá user " + rowview["ROLE"].ToString(), "Thông báo", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                    //    {
+                    //        string rolename = rowview["ROLE"].ToString();
+                    //        string sql = "ALTER SESSION SET \"_ORACLE_SCRIPT\" = true";
+                    //        Class.DB_Config.RunSqlDel(sql);
+                    //        sql = "DROP ROLE " + rolename;
+                    //        Class.DB_Config.RunSqlDel(sql);
+                    //        role_lb_errorout.Content = "Xóa thành công!!!";
+                    //        role_lb_errorout.Background = Brushes.LightGreen;
+                    //        Role_HienThi();
+                    //    }
+
+                    //}
+                    //else
+                    //{
+                    //    role_lb_errorout.Content = "Bạn chưa chọn role để xóa!!!";
+                    //    role_lb_errorout.Background = Brushes.IndianRed;
+                    //}
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        public void role_xemquyen_tab_hienThi()
+        {
+            try
+            {
+                DataTable tbl = new DataTable();
+                string sql = "select* from ROLE_TAB_PRIVS where ROLE='" + role + "'";
+                tbl = Class.DB_Config.GetDataToTable(sql);
+                Role_xq_tab_Datagrid.ItemsSource = null;
+                Role_xq_tab_Datagrid.ItemsSource = tbl.DefaultView;
+                Role_xq_tab_lb.Content = "Quyền trên tab của role: " + role;
+
+            }
+            catch { }
+        }
+        private void Xemquyen_sys_loaded(object sender, RoutedEventArgs e)
+        {
+            role_xemquyen_tab_hienThi();
+        }
+
+        private void Role_xq_tab_Datagrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            role_xemquyen_tab_hienThi();
+        }
+
+        private void role_xq_tab_bt_quaylai_click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void role_xq_tab_bt_xoa_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Role_xq_tab_Datagrid.SelectedIndex.ToString() != null)
+                {
+                    DataRowView rowview = (DataRowView)Role_xq_sys_Datagrid.SelectedItem;
+                    if (rowview != null)
+                    {
+                        if (MessageBox.Show("Xác nhận xoá quyền " + rowview["PRIVILEGE"].ToString()+ " trên "+ rowview["TABLE_NAME"].ToString()+" của "+role, "Thông báo", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                        {
+                            string rolename = rowview["PRIVILEGE"].ToString();
+                            string sql = "ALTER SESSION SET \"_ORACLE_SCRIPT\" = true";
+                            Class.DB_Config.RunSqlDel(sql);
+                            sql = "REVOKE " + rowview["PRIVILEGE"].ToString() + " ON " + rowview["TABLE_NAME"].ToString() + " FROM " + role ;
+                            Class.DB_Config.RunSqlDel(sql);
+                            role_xq_tab_lb_errorout.Content = "Xóa thành công!!!";
+                            role_xq_tab_lb_errorout.Background = Brushes.LightGreen;
+                           
+                        }
+
+                    }
+                    else
+                    {
+                        role_xq_tab_lb_errorout.Content = "Bạn chưa chọn role để xóa!!!";
+                        role_xq_tab_lb_errorout.Background = Brushes.IndianRed;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void role_xq_bt_tab_lammoi_click(object sender, RoutedEventArgs e)
+        {
+            role_xemquyen_tab_hienThi();
         }
     }
 }
---
+++ ATBM_QuanLiDeAn/PH1/Role_capquyen.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ATBM_QuanLiDeAn.PH1
+{
+    /// <summary>
+    /// Interaction logic for Role_capquyen.xaml
+    /// </summary>
+    public partial class Role_capquyen : Window
+    {
+        public string role;
+        public Role_capquyen(string role)
+        {
+            InitializeComponent();
+            this.role = role;
+            role_cq_lb.Content = "Role: " + role;
+            DSRole();
+        }
+        private void DSRole()
+        {
+            string sql = "SELECT object_name\r\nFROM all_objects\r\nwhere owner in (SELECT username FROM dba_users\r\n                where default_tablespace = 'USERS' and EXpiry_date is not null) and object_type='TABLE'";
+            DataTable dt = new DataTable();
+            dt = Class.DB_Config.GetDataToTable(sql);
+            role_cq_cb_tab.Items.Clear();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                role_cq_cb_tab.Items.Add(dt.Rows[i]["OBJECT_NAME"]);
+            }
+            
+        }
+       
+
+        private void Role_cq_bt_quaylai_click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Role_cq_bt_gan_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(role_cq_cb_tab.Text=="" || role_cq_cb_tab.Text == "")
+                {
+                    role_cq_lb_errorout.Content = "Có trường rỗng!!!";
+                    role_cq_lb_errorout.Background = Brushes.IndianRed;
+                }
+                else
+                {
+                    string sql = "ALTER SESSION SET \"_ORACLE_SCRIPT\" = true";
+                    Class.DB_Config.RunSqlDel(sql);
+                    sql = "GRANT " + role_cq_cb_quyen.Text + " ON " + role_cq_cb_tab.Text + " TO " + role;
+                    Class.DB_Config.RunSqlDel(sql);
+                    role_cq_lb_errorout.Content = "Cap quyen thành công!!!";
+                    role_cq_lb_errorout.Background = Brushes.LightGreen;
+                }
+                
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
